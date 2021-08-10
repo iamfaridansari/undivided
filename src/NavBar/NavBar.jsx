b8df0d50b47de492bafc6e73d0166f29de045863
iamfaridansari <82579834+iamfaridansari@users.noreply.github.com>
2021-08-10 11:11:17 +0530

    back to normal

--- src/NavBar/NavBar.jsx
+++ src/NavBar/NavBar.jsx
@@ -25,7 +25,7 @@ const NavBar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <NavLink className="nav-link" to="/undivided/">
+                <NavLink className="nav-link" to="/">
                   Form
                 </NavLink>
               </li>
